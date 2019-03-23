3f4a40b11049891ef54648f314efbe380268a1b4
AkuiChan <mfly16@student.aau.dk>
2019-03-24 00:40:46 +0100

    initial convo in all lvls

--- Assets/Scripts/activateOnLoad.cs
+++ Assets/Scripts/activateOnLoad.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class activateOnLoad : MonoBehaviour
 {
+    [SerializeField]
+    GameEvent _event;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (_event != null)
+        {
+            _event.Invoke();
+        }
     }
 
     // Update is called once per frame
