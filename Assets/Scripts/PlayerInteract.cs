e42f80a3ef6442902c567ef50caed9913b12181f
Jackie Engberg Christensen <jackie@e-komplet.dk>
2019-03-24 02:38:04 +0100

    Player Interact Script

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask _mask;
+
+    Vector2 _lookDir = new Vector2(0, -1);
+    float x, y = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            x = -1;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            x = 1;
+        }
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            x = 0;
+        }
+
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            y = 1;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            y = -1;
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            y = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _lookDir = new Vector2(x, y);
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, _lookDir, 5, _mask);
+            if (hit.transform.GetComponent<IInteractable>() != null)
+            {
+                hit.transform.GetComponent<IInteractable>().Interact();
+            }
+        Debug.DrawRay(transform.position, _lookDir, Color.red);
+        }
+    }
+}
